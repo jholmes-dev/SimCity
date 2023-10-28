2dd0b720024d45ad23ea8b2b41e0a698ff20c413
Jeffrey Holmes <coffeman500@gmail.com>
2023-10-28 01:40:50 -0500

    Finalized growth tests across map cell classes

--- SimCityTest/IndustrialCellTest.cpp
+++ SimCityTest/IndustrialCellTest.cpp
@@ -1,15 +1,15 @@
 #include "pch.h"
-#include "../include/ResidentialCell.h"
-#include "../src/ResidentialCell.cpp"
+#include "../include/IndustrialCell.h"
+#include "../src/IndustrialCell.cpp"
 
-class ResidentialCellTest : public ::testing::Test
+class IndustrialCellTest : public ::testing::Test
 {
 protected:
 	void SetUp()
 	{
-		rc = new ResidentialCell(map, 'R', 0, 0);
+		rc = new IndustrialCell(map, 'I', 0, 0);
 		qc = new QueueController(w, g);
-		
+
 		w = 2;
 		g = 2;
 		rc->adjPowerPlants = 10;
@@ -23,108 +23,97 @@ protected:
 	}
 
 	std::vector<std::vector<MapCell*>> map;
-	ResidentialCell* rc = NULL;
+	IndustrialCell* rc = NULL;
 	QueueController* qc = NULL;
 	int w = 0;
 	int g = 0;
 };
 
 
-TEST_F(ResidentialCellTest, ZeroPopGrowth)
+TEST_F(IndustrialCellTest, ZeroPopGrowth)
 {
 	rc->population = 0;
 
-	// Adj powerline
+	// Adj power line
 	EXPECT_EQ(rc->step(w, g, qc), true);
 
 	// No powerlines or pop
 	rc->adjPowerLines = 0;
 	EXPECT_EQ(rc->step(w, g, qc), false);
 
-	// Adjacent population
+	// Test population
 	rc->adjPop[1] = 1;
 	EXPECT_EQ(rc->step(w, g, qc), true);
 	rc->adjPop[1] = 10;
 	EXPECT_EQ(rc->step(w, g, qc), true);
+
+	// Insufficient workers
+	w = 1;
+	EXPECT_EQ(rc->step(w, g, qc), false);
+
+	// No available workers
+	w = 0;
+	EXPECT_EQ(rc->step(w, g, qc), false);
 }
 
 
 
-TEST_F(ResidentialCellTest, OnePopGrowth)
+TEST_F(IndustrialCellTest, OnePopGrowth)
 {
 	rc->population = 1;
+
+	// No adj pop
 	EXPECT_EQ(rc->step(w, g, qc), false);
 
-	// Test adjacent population
+	// Test population
 	rc->adjPop[1] = 1;
 	EXPECT_EQ(rc->step(w, g, qc), false);
-
 	rc->adjPop[1] = 2;
 	EXPECT_EQ(rc->step(w, g, qc), true);
-
 	rc->adjPop[1] = 10;
 	EXPECT_EQ(rc->step(w, g, qc), true);
+
+	// Insufficient workers
+	w = 1;
+	EXPECT_EQ(rc->step(w, g, qc), false);
+
+	// No available workers
+	w = 0;
+	EXPECT_EQ(rc->step(w, g, qc), false);
 }
 
 
 
-TEST_F(ResidentialCellTest, TwoPopGrowth)
+TEST_F(IndustrialCellTest, TwoPopGrowth)
 {
 	rc->population = 2;
+
+	// No adj pop
 	EXPECT_EQ(rc->step(w, g, qc), false);
 
-	// Test adjacent population
+	// Test population
 	rc->adjPop[2] = 3;
 	EXPECT_EQ(rc->step(w, g, qc), false);
-
 	rc->adjPop[2] = 4;
 	EXPECT_EQ(rc->step(w, g, qc), true);
-
 	rc->adjPop[2] = 10;
 	EXPECT_EQ(rc->step(w, g, qc), true);
+
+	// Insufficient workers
+	w = 1;
+	EXPECT_EQ(rc->step(w, g, qc), false);
+
+	// No available workers
+	w = 0;
+	EXPECT_EQ(rc->step(w, g, qc), false);
 }
 
 
 
-TEST_F(ResidentialCellTest, ThreePopGrowth)
+TEST_F(IndustrialCellTest, ThreePopGrowth)
 {
+	// Cell should never grow from 3 pop
 	rc->population = 3;
-	EXPECT_EQ(rc->step(w, g, qc), false);
-
-	// Test adjacent population
-	rc->adjPop[3] = 5;
-	EXPECT_EQ(rc->step(w, g, qc), false);
-
-	rc->adjPop[3] = 6;
-	EXPECT_EQ(rc->step(w, g, qc), true);
-
-	rc->adjPop[3] = 10;
-	EXPECT_EQ(rc->step(w, g, qc), true);
-}
-
-
-
-TEST_F(ResidentialCellTest, FourPopGrowth)
-{
-	rc->population = 4;
-	EXPECT_EQ(rc->step(w, g, qc), false);
-
-	// Test adjacent population
-	rc->adjPop[4] = 7;
-	EXPECT_EQ(rc->step(w, g, qc), false);
-
-	rc->adjPop[4] = 8;
-	EXPECT_EQ(rc->step(w, g, qc), true);
-
-	rc->adjPop[4] = 10;
-	EXPECT_EQ(rc->step(w, g, qc), true);
-}
-
-
-
-TEST_F(ResidentialCellTest, FivePopGrowth)
-{
-	rc->population = 5;
 	w = 10;
 	g = 10;
 	rc->adjPowerPlants = 10;
